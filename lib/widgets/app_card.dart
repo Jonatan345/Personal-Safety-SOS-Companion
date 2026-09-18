524707d7fbef19698690995ca24d32f6cdb6d61e
Jonatan345 <imanuel.jonatan@student.pradita.ac.id>
2026-09-18 22:35:54 +0700

    Fix BoxShadow color opacity method

--- lib/widgets/app_card.dart
+++ lib/widgets/app_card.dart
@@ -17,7 +17,7 @@ class AppCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(18),
         border: Border.all(color: AppColors.border),
         boxShadow: [
-          BoxShadow(color: Colors.black.withOpacity(0.03), blurRadius: 12, offset: const Offset(0, 4)),
+          BoxShadow(color: Colors.black.withValues(alpha: 0.03), blurRadius: 12, offset: const Offset(0, 4)),
         ],
       ),
       child: child,
